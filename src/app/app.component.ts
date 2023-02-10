6e53ea6331c6a8c6bfbe259e2395fb49667be9b0
Nagaraju Makanaboyina <nagaraju.makanaboyina@peopletech.com>
2023-02-10 17:52:27 +0530

    doughunt chart updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,27 +21,41 @@ export class AppComponent {
       this.ctx = this.canvas.getContext('2d');
 
       new Chart(this.ctx, {
-          type: 'polarArea',
-          data: {
-            labels: [
-              'Red',
-              'Green',
-              'Yellow',
-              'Grey',
-              'Blue'
-            ],
+          type: 'doughnut',
+          // data: {
+          //   labels: [
+          //     'Red',
+          //     'Green',
+          //     'Yellow',
+          //     'Grey',
+          //     'Blue'
+          //   ],
+          //   datasets: [{
+          //     label: 'My First Dataset',
+          //     data: [11, 16, 7, 3, 14],
+          //     backgroundColor: [
+          //       'rgb(255, 99, 132)',
+          //       'rgb(75, 192, 192)',
+          //       'rgb(255, 205, 86)',
+          //       'rgb(201, 203, 207)',
+          //       'rgb(54, 162, 235)'
+          //     ]
+          //   }]
+          // },
+          data : {
             datasets: [{
-              label: 'My First Dataset',
-              data: [11, 16, 7, 3, 14],
-              backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(75, 192, 192)',
-                'rgb(255, 205, 86)',
-                'rgb(201, 203, 207)',
-                'rgb(54, 162, 235)'
-              ]
-            }]
-          },
+                data: [10, 20, 30,40,50]
+            }],
+        
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Red',
+                'Yellow',
+                'Blue',
+                'green',
+                'pink'
+            ]
+        },
       });
   }
 
